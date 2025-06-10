67063f7551aba64f754a70d92b4a2bca37f4b2d3
OmerFarooq246 <ofarooq.bscs21seecs@seecs.edu.pk>
2025-06-10 16:49:04 +0500

    orm for models

--- app/models/users.py
+++ app/models/users.py
@@ -1,12 +1,20 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, DateTime
 from app.db.database import Base
 from datetime import datetime, timezone
 
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True) #index and auto_increment true by default
-    username = Column(String, unique=True, nullable=False, index=True)
-    password = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)) #lambda will cause to run on every new row
-    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True) #index and auto_increment true by default
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc)
+    ) #lambda will cause to run on every new row
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
